3e13de2619f441e0efb18bf1c2cba32da7742aac
David <david.tapiador@envato.com>
2016-03-26 02:13:26 +1100

    Add tests for parameters and functions

--- spec/integration/raml_spec/resource_type_and_trait_spec.rb
+++ spec/integration/raml_spec/resource_type_and_trait_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Resource Types and Traits RAML SPEC examples', type: :raml_spec do
-  describe 'resource_type_and_trait_merging' do
+  describe 'resource_type_and_traits_merging' do
     describe 'users resource' do
       let(:users) { raml.root.resources.fetch('/users') }
 
@@ -11,7 +11,7 @@ describe 'Resource Types and Traits RAML SPEC examples', type: :raml_spec do
         its(:query_parameters) do
           is_expected.to have_item('numPages')
             .with_values(
-              description: "The number of pages to return, not to exceed <<maxPages>>"
+              description: "The number of pages to return, not to exceed 10"
             )
         end
 
@@ -39,7 +39,7 @@ describe 'Resource Types and Traits RAML SPEC examples', type: :raml_spec do
         end
       end
 
-      describe 'get method, applies only the resource trait' do
+      describe 'post method, applies only the resource trait' do
         subject { users.methods.fetch('post') }
 
         its(:query_parameters) { is_expected.to be nil }
@@ -52,6 +52,62 @@ describe 'Resource Types and Traits RAML SPEC examples', type: :raml_spec do
               example: "5757gh76",
               required: true
             )
+        end
+      end
+    end
+  end
+
+  describe 'resource_type_and_traits_parameters' do
+    describe '/books resource' do
+      let(:books) { raml.root.resources.fetch('/books') }
+
+      describe 'get method, applies all the traits and resource type' do
+        subject { books.methods.fetch('get') }
+
+        its(:query_parameters) do
+          is_expected.to have_item('title').with_values(
+            description: "Return books that have their title matching the given value"
+          )
+        end
+
+        its(:query_parameters) do
+          is_expected.to have_item('access_token').with_values(
+            description: "A valid access_token is required"
+          )
+        end
+
+        its(:query_parameters) do
+          is_expected.to have_item('numPages').with_values(
+            description: "The number of pages to return, not to exceed 10"
+          )
+        end
+      end
+    end
+  end
+
+  describe 'resource_type_and_traits_functions' do
+    describe '/userId resource' do
+      let(:userId) { raml.root.resources.fetch('/userId') }
+
+      describe 'GET method' do
+        subject { userId.methods.fetch('get') }
+
+        its(:description) do
+          is_expected.to eq <<-TXT.strip_heredoc
+            This should cover all the functions:
+            userId as userId
+            userIds as userIds
+            USERID as USERID
+            userid as userid
+            userId as userId
+            UserId as UserId
+            user_id as user_id
+            USER_ID as USER_ID
+            user-id as user-id
+            USER-ID as USER-ID
+            complex one user-id user-id
+
+          TXT
         end
       end
     end
