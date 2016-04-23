91093da0403ccbb5ff8786d8ba4ddf5a44e46a93
David <david.tapiador@envato.com>
2016-04-23 16:29:18 +0200

    start raml type spec integration tests

---
+++ spec/integration/raml_spec/raml_type_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Resource RAML SPEC examples', type: :raml_spec do
+  describe 'raml_type_property_declaration' do
+    let(:person) { raml.root.types.fetch('Person') }
+    subject { person }
+
+    its(:type) { is_expected.to eq('object') }
+
+    describe 'properties #name' do
+      subject { person.properties.fetch('name') }
+
+      its(:type)     { is_expected.to eq('string') }
+      its(:required) { is_expected.to eq(true) }
+    end
+
+    describe 'properties #age' do
+      subject { person.properties.fetch('age') }
+
+      its(:type)     { is_expected.to eq('number') }
+      its(:required) { is_expected.to eq(false) }
+    end
+  end
+end
