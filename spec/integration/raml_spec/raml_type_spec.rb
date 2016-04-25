f2e24fbb5c35d6a8604171d230a03b485aba3a1e
David <david.tapiador@envato.com>
2016-04-25 07:51:48 +0200

    Add basic inheritance tests

--- spec/integration/raml_spec/raml_type_spec.rb
+++ spec/integration/raml_spec/raml_type_spec.rb
@@ -42,4 +42,38 @@ describe 'Resource RAML SPEC examples', type: :raml_spec do
       its(:required) { is_expected.to eq(false) }
     end
   end
+
+  describe 'raml_type_inheritance' do
+    describe 'Type Person' do
+      let(:person) { raml.root.types.fetch('Person') }
+      subject { person }
+
+      its(:type) { is_expected.to eq('object') }
+
+      describe 'properties #name' do
+        subject { person.properties.fetch('name') }
+
+        its(:type)     { is_expected.to eq('string') }
+      end
+    end
+
+    describe 'Type Employee' do
+      let(:person) { raml.root.types.fetch('Employee') }
+      subject { person }
+
+      its(:type) { is_expected.to eq('Person') }
+
+      describe 'properties #id' do
+        subject { person.properties.fetch('id') }
+
+        its(:type)     { is_expected.to eq('string') }
+      end
+
+      describe 'properties #name' do
+        subject { person.properties.fetch('name') }
+
+        its(:type)     { is_expected.to eq('string') }
+      end
+    end
+  end
 end
