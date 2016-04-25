631f6852256f770b11218219266a29695df5906d
David <david.tapiador@envato.com>
2016-04-25 07:21:31 +0200

    Improve property declaration syntax shortcuts

--- spec/integration/raml_spec/raml_type_spec.rb
+++ spec/integration/raml_spec/raml_type_spec.rb
@@ -21,4 +21,25 @@ describe 'Resource RAML SPEC examples', type: :raml_spec do
       its(:required) { is_expected.to eq(false) }
     end
   end
+
+  describe 'raml_type_property_declaration_alternative_syntax' do
+    let(:person) { raml.root.types.fetch('Person') }
+    subject { person }
+
+    its(:type) { is_expected.to eq('object') }
+
+    describe 'properties #name' do
+      subject { person.properties.fetch('name') }
+
+      its(:type)     { is_expected.to eq('string') }
+      its(:required) { is_expected.to eq(true) }
+    end
+
+    describe 'properties #age' do
+      subject { person.properties.fetch('age') }
+
+      its(:type)     { is_expected.to eq('number') }
+      its(:required) { is_expected.to eq(false) }
+    end
+  end
 end
