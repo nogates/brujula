8a8578eb51e648317e4387405d6034fc3a5729dc
David <david.tapiador@envato.com>
2016-04-25 07:56:47 +0200

    Adding tests for multiple inheritance

--- spec/integration/raml_spec/raml_type_spec.rb
+++ spec/integration/raml_spec/raml_type_spec.rb
@@ -58,21 +58,74 @@ describe 'Resource RAML SPEC examples', type: :raml_spec do
     end
 
     describe 'Type Employee' do
-      let(:person) { raml.root.types.fetch('Employee') }
-      subject { person }
+      let(:employee) { raml.root.types.fetch('Employee') }
+      subject { employee }
 
       its(:type) { is_expected.to eq('Person') }
 
       describe 'properties #id' do
-        subject { person.properties.fetch('id') }
+        subject { employee.properties.fetch('id') }
 
         its(:type)     { is_expected.to eq('string') }
       end
 
       describe 'properties #name' do
-        subject { person.properties.fetch('name') }
+        subject { employee.properties.fetch('name') }
 
         its(:type)     { is_expected.to eq('string') }
+      end
+    end
+  end
+
+  describe 'raml_type_multiple_inheritance' do
+    describe 'Type Person' do
+      let(:person) { raml.root.types.fetch('Person') }
+      subject { person }
+
+      its(:type) { is_expected.to eq('object') }
+
+      describe 'properties #name' do
+        subject { person.properties.fetch('name') }
+
+        its(:type) { is_expected.to eq('string') }
+      end
+    end
+
+    describe 'Type EmailOwner' do
+      let(:email_owner) { raml.root.types.fetch('EmailOwner') }
+      subject { email_owner }
+
+      its(:type) { is_expected.to eq('object') }
+
+      describe 'properties #email' do
+        subject { email_owner.properties.fetch('email') }
+
+        its(:type) { is_expected.to eq('string') }
+      end
+    end
+
+    describe 'Type Employee' do
+      let(:employee) { raml.root.types.fetch('Employee') }
+      subject { employee }
+
+      its(:type) { is_expected.to eq([ "Person", "EmailOwner" ]) }
+
+      describe 'properties #id' do
+        subject { employee.properties.fetch('id') }
+
+        its(:type) { is_expected.to eq('string') }
+      end
+
+      describe 'properties #name' do
+        subject { employee.properties.fetch('name') }
+
+        its(:type) { is_expected.to eq('string') }
+      end
+
+      describe 'properties #email' do
+        subject { employee.properties.fetch('email') }
+
+        its(:type) { is_expected.to eq('string') }
       end
     end
   end
